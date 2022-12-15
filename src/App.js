9528ccdfee57bbc6046c90e0e0f8735b4aafebcb
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2022-12-15 16:47:29 -0600

    [DELETE] Resume component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Head/Header';
 import Hero from './components/Hero/Hero';
 import Portfolio from './components/Portfolio/Portfolio';
-import Resume from './components/Resume/Resume';
 import Testimonial from './components/Testimonial/Testimonial';
 import ParticlesBG from './components/ParticlesBG.js';
 
