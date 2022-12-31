09696174a6783594714a71b393924fc49c1c23e9
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2022-12-31 14:18:45 -0600

    [FIX] AllProtfolioProjects scroll height fixed

--- src/components/Portfolio/PortfolioData.js
+++ src/components/Portfolio/PortfolioData.js
@@ -32,6 +32,7 @@ const data = [
     desc: `Mi sitio web personal estilo Neumorfismo.
      Este proyecto me tomo mucho trabajo solo para poder expresar lo que hago y lo que me gusta hacer. 
      Este portafolio es una PWA, asi que puedes instalar la aplicacion en tu dispositivo.
+    
      Ademas viene con un pequeño blog usando markdown. Espero que te guste <3.`,
     links: {
       code: 'https://github.com/Tono2007/portafolio',
